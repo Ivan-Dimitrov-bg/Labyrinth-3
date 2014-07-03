17a508cabd3ddd4634ed90c97aabce3c82bb9c17
yavor <iavorsk@gmail.com>
2014-07-03 10:08:38 +0300

    Added 4 LabCreator classes for factory pattern

---
+++ GameEngine/LabCreator.cs
@@ -0,0 +1,11 @@
+﻿namespace Labyrinth.GameEngine
+{
+    using Labyrinth.GameObjects;
+    using System;
+    using System.Linq;
+
+    public abstract class LabCreator
+    {
+        public abstract Maze CreateLabyrinth();
+    }
+}
