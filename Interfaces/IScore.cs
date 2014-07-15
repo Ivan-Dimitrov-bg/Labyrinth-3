725105b4aeb726d03b94ae604705594c1c89310a
Sh0nZi <sss17@mail.com>
2014-07-15 09:59:16 +0300

    IScore interface introduced...

---
+++ Interfaces/IScore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Labyrinth.Interfaces
+{
+    public interface IScore : IRenderable
+    {
+        int Moves { get; set; }
+
+        int Position { get; set; }
+
+        string Name { get; set; }
+    }
+}
