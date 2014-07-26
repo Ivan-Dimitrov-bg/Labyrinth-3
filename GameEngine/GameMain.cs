34b6502a496d1c9f3f838972d6b5742708be7acc
Sh0nZi <sss17@mail.com>
2014-07-27 01:11:41 +0300

    Fixes

--- GameEngine/GameMain.cs
+++ GameEngine/GameMain.cs
@@ -1,8 +1,6 @@
-﻿using Labyrinth.GameObjects;
-using Labyrinth.Interfaces;
-
-namespace Labyrinth.GameEngine
+﻿namespace Labyrinth.GameEngine
 {
+    
     /// <summary>
     /// GameMain class
     /// <remarks>
@@ -14,7 +12,6 @@ namespace Labyrinth.GameEngine
         public static void Main()
         {
             // Facade pattern
-            PlayerCommand command = PlayerCommand.Exit;
             LabyrinthGame game = new LabyrinthGame();
             game.Start();
         }
